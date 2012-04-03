89b0d679bcaf9853fb458a706930cba337583976
Mikael Lof <mickes511@hotmail.com>
2012-04-03 11:28:56 +0200

    test

---
+++ JSF_Hello_World/src/main/java/Load.java
@@ -0,0 +1,43 @@
+public class Load
+{
+
+
+
+
+    private int number;
+
+
+
+    private String name;
+    public Load(int number, String name)
+    {
+        this.number = number;
+        this.name = name;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+    
+    public int getNumber()
+    {
+        return number;
+    }
+
+    public void setNumber(int number)
+    {
+        this.number = number;
+    }
+    
+    
+
+
+    
+
+}
---
+++ JSF_Hello_World/src/main/java/StartPage.java
@@ -0,0 +1,91 @@
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import java.io.Serializable;
+import java.util.ArrayList;
+
+@ManagedBean
+@SessionScoped
+public class StartPage implements Serializable {
+
+
+
+    ArrayList<Load> loads = new ArrayList<Load>();
+    private String name;
+    int number = 5;
+    private boolean vis = true;
+
+    public String getTempLoadName()
+    {
+        return tempLoadName;
+    }
+
+
+
+    public int getTempLoadNumber()
+    {
+        return tempLoadNumber;
+    }
+
+    public void setTempLoadNumber(int tempLoadNumber)
+    {
+        this.tempLoadNumber = tempLoadNumber;
+    }
+
+    private int tempLoadNumber;
+
+    public void setTempLoadName(String tempLoadName)
+    {
+        this.tempLoadName = tempLoadName;
+    }
+
+    private String tempLoadName ="";
+    private static final long serialVersionUID = 1L;
+
+    public void test()
+    {
+        loads.add(new Load(tempLoadNumber,tempLoadName ));
+    }
+
+    public ArrayList<Load> getLoads()
+    {
+        return loads;
+    }
+
+    public void setLoads(ArrayList<Load> loads)
+    {
+        this.loads = loads;
+    }
+
+
+    public boolean isVis()
+    {
+        return vis;
+    }
+
+    public void setVis(boolean vis)
+    {
+        this.vis = vis;
+    }
+
+
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getNumber()
+    {
+        return number;
+    }
+
+    public void setNumber(int number)
+    {
+        this.number = number;
+    }
+
+
+
+}
