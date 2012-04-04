8f6149373b3dfe51caddf866eafad00202363f83
Mikael Lof <mickes511@hotmail.com>
2012-04-04 14:29:27 +0200

    12

---
+++ Load_Planner/main/src/main/java/showBeans/RegisterLoadsManageBean.java
@@ -0,0 +1,10 @@
+package showBeans;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+@ManagedBean
+@RequestScoped
+public class RegisterLoadsManageBean {
+
+}
---
+++ Load_Planner/main/src/main/java/showBeans/ShowLoadsManageBean.java
@@ -0,0 +1,12 @@
+package showBeans;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import javax.faces.bean.SessionScoped;
+
+@ManagedBean
+@RequestScoped
+public class ShowLoadsManageBean {
+
+
+}
